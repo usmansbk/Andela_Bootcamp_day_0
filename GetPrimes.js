e328e565b9bc3eef975a45c15d06e3b86b7eec9d
Usman Suleiman <usmansbk@gmail.com>
2017-05-08 20:24:06 +0100

    Add Check for edge cases and Optimization

--- GetPrimes.js
+++ GetPrimes.js
@@ -1,7 +1,11 @@
 
 const getPrimes = (n)=> {
+	if (isNaN(n) || n <= 0) return undefined
 	let array = [];
 	for (let number = 2; number <= n; number++) {
+		if (number % 2 == 0 && number > 2) {
+			continue;
+		}
 		let isPrime = true;
 		for (let counter = 2; counter <= Math.trunc(Math.sqrt(number)); counter++) {
 			if (number % counter == 0) {
@@ -15,3 +19,5 @@ const getPrimes = (n)=> {
 	}
 	return array;
 }
+
+console.log(getPrimes(20));
